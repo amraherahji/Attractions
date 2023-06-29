1d367c139147e523b81444baab5d7348faa71b25
Amrah Rahji <arasheedrahji@gmail.com>
2023-06-29 10:39:04 -0400

    Initial Commit

---
+++ Attractions/AttractionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttractionsApp.swift
+//  Attractions
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AttractionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
